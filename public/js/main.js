291900b7afab9fc4bbe1f6558ec8bf70754bffad
Alex Straughn <97370656+astraughn5@users.noreply.github.com>
2022-07-28 13:09:37 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,17 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //set the variable that contains all the delete buttons
+const item = document.querySelectorAll('.item span') //dont see where this is used
+const itemCompleted = document.querySelectorAll('.item span.completed') //grab the completed items
 
 Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
+    element.addEventListener('click', deleteItem) //add to each button the deleteItem function
 })
 
 Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
+    element.addEventListener('click', markComplete) //add to each button the mark complete function
 })
 
 Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
+    element.addEventListener('click', markUnComplete) //add to each button the mark un complete function
 })
 
 async function deleteItem(){
